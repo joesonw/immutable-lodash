2e2d69f0d6cfcb02b02b0e4319fae7fd8381b9da
Qiaosen Huang <joesonw@gmail.com>
2016-07-20 16:47:52 +0800

    rename describe style.

--- test/array/chunk.js
+++ test/array/chunk.js
@@ -2,7 +2,7 @@
 * @Author: Qiaosen Huang
 * @Date:   2016-07-20 16:04:43
 * @Last Modified by:   Qiaosen Huang
-* @Last Modified time: 2016-07-20 16:35:40
+* @Last Modified time: 2016-07-20 16:47:24
 */
 
 'use strict';
@@ -11,19 +11,15 @@ const Immutable = require('immutable');
 const fromJS = Immutable.fromJS;
 const equal = require('../../test-util').equal;
 
-
-
-describe('array', () => {
-    describe('chunk', () => {
-        it('should split array into size-of-2 arrays', () => {
-            const array = fromJS(['a', 'b', 'c', 'd']);
-            const result = _.chunk(array, 2);
-            equal(result ,fromJS([['a', 'b'], ['c', 'd']]));
-        })
-        it('should split array into size-of-3 arrays', () => {
-            const array = fromJS(['a', 'b', 'c', 'd']);
-            const result = _.chunk(array, 3);
-            equal(result ,fromJS([['a', 'b', 'c'], ['d']]));
-        })
+describe('_.chunk', () => {
+    it('should split array into size-of-2 arrays', () => {
+        const array = fromJS(['a', 'b', 'c', 'd']);
+        const result = _.chunk(array, 2);
+        equal(result ,fromJS([['a', 'b'], ['c', 'd']]));
+    })
+    it('should split array into size-of-3 arrays', () => {
+        const array = fromJS(['a', 'b', 'c', 'd']);
+        const result = _.chunk(array, 3);
+        equal(result ,fromJS([['a', 'b', 'c'], ['d']]));
     })
 })
