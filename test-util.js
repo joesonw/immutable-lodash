dbd049bc66a45e43e8018d39df342ebce288004d
Qiaosen Huang <joesonw@gmail.com>
2016-07-20 17:02:23 +0800

    4 spaces instead of 2 spaces for JSON.stringify errors.

--- test-util.js
+++ test-util.js
@@ -2,7 +2,7 @@
 * @Author: Qiaosen Huang
 * @Date:   2016-07-20 16:27:11
 * @Last Modified by:   Qiaosen Huang
-* @Last Modified time: 2016-07-20 16:30:22
+* @Last Modified time: 2016-07-20 17:02:14
 */
 
 'use strict';
@@ -13,9 +13,9 @@ module.exports = {
     equal: (lhs, rhs) => {
         if (!is(lhs, rhs)) {
             throw new Error('AssertionError: expcted \n' + 
-                JSON.stringify(lhs.toJSON(), 0, 2) +
+                JSON.stringify(lhs.toJSON(), 0, 4) +
                 '\n to euqlas \n' +
-                JSON.stringify(rhs.toJSON(), 0, 2));
+                JSON.stringify(rhs.toJSON(), 0, 4));
         }
     },
 };
