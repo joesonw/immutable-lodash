101fb61a49417b3f8133c7d572ac958092a6ed8a
Qiaosen Huang <joesonw@gmail.com>
2016-07-20 17:20:03 +0800

    refine package.json

--- src/array/chunk.js
+++ src/array/chunk.js
@@ -2,21 +2,25 @@
 * @Author: Qiaosen Huang
 * @Date:   2016-07-20 15:41:35
 * @Last Modified by:   Qiaosen Huang
-* @Last Modified time: 2016-07-20 16:43:14
+* @Last Modified time: 2016-07-20 17:02:04
 */
 
 'use strict';
 const List = require('immutable').List;
 
-module.exports = function(array, size) {
-    size = size || 1;
+module.exports = function(array, size, iter) {
+    size = size || 0;
+    size = parseInt(size);
+    if (iter) {
+        size = 0;
+    }
     let result = List(); 
     let i = 0;
     let list = List();
     array.forEach((value) => {
         list = list.push(value);
         i++;
-        if (i % size === 0) {
+        if ((i % size || 0) == 0) {
             result = result.push(list);
             list = List();
         }
